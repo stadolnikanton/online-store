26e9c8b59604b3d4463ddfa7fd4cfe0b980bc702
Anton Stadolnik <stadonikanton1@gmail.com>
2025-11-23 00:29:13 +0300

    Добавлена погинация без стилей

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
 
-from shop.models import Product, ProductType 
+from shop.models import Product
 
 class IndexView(View):
     def get(self, request):
         products = Product.objects.all()
-        context = {"products": products}
+        paginator = Paginator(products, 9)
+        
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {
+                "products": products, 
+                "page_obj": page_obj
+                }
         return render(request, "pages/index.html", context) 
 
 
