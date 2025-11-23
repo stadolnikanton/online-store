18a0e687495e1dbf888c3432026d951b16f9659c
Anton Stadolnik <stadonikanton1@gmail.com>
2025-11-23 20:04:12 +0300

    Правка мелких ошибок

--- api/views.py
+++ api/views.py
@@ -18,7 +18,7 @@ class ProductDetailsAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, pk):
+    def get(self, request, pk):
         try:
             product = Product.objects.get(pk=pk)
         except Product.DoesNotExist:
@@ -32,7 +32,7 @@ class ProductDetailsAPIView(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
-        product = self.get_object(pk)
+        product = Product.objects.get(pk=pk)
 
         if not product:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
@@ -47,7 +47,7 @@ class ProductDetailsAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        product = self.get_object(pk)
+        product = Product.objects.get(pk=pk)
 
         if not product:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
