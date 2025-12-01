9e15ff760071fa1c6ee22b539559f71e478e3f47
Anton Stadolnik <stadonikanton1@gmail.com>
2025-12-01 18:57:15 +0300

    Начало работы над api курса валют и начало работы над оповещениями на почту

---
+++ api/task.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+from celery import shared_task
+from django.core.mail import send_mail
+
+logger = logging.getLogger("api")
+
+
+@shared_task  # В разработке
+def get_exchange_rate():
+    response = requests.get("https://api.nbrb.by/exrates/rates?periodicity=0")
+    response = response.json()
+    for res in response:
+        print(res)
+    return response
+
+
+# В разработке
+# send_mail("Subject here","Here is the message.","from@example.com",["to@example.com"], fail_silently=False,)
